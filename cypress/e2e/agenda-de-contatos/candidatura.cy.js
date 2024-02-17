45e06a1762673c9f67ff2c6cce50ff649819b95c
YuriDonato <yuridonatosis@gmail.com>
2024-02-17 15:33:29 -0300

    done

--- cypress/e2e/agenda-de-contatos/candidatura.cy.js
+++ cypress/e2e/agenda-de-contatos/candidatura.cy.js
@@ -5,25 +5,30 @@ describe("Testes para a agenda de contatos", () => {
         cy.visit("https://agenda-contatos-react.vercel.app/");
     });
     it("Deve cadastrar um contato", () => {
-        cy.visit("https://agenda-contatos-react.vercel.app/");
         cy.get('[type="text"]').type("Yuri Donato");
         cy.get('[type="email"]').type("yuri@teste.com");
         cy.get('[type="tel"]').type("67 992718050");
         cy.get(".adicionar").click();
+        cy.wait(2000);
+        cy.screenshot("tela-cadastro-concluido", { overwrite: true });
     });
 
     it("Deve alterar o contato", () => {
         cy.get(":nth-child(5) > .sc-gueYoa > .edit").click();
-        cy.get('[type="text"]').clear("Yuri Donato Editado");
-        cy.get('[type="email"]').clear("yuriEditado@teste.com");
-        cy.get('[type="tel"]').clear("68 992718050");
+        cy.get('[type="text"]').clear();
+        cy.get('[type="email"]').clear();
+        cy.get('[type="tel"]').clear();
         cy.get('[type="text"]').type("Yuri Donato Editado");
         cy.get('[type="email"]').type("yuriEditado@teste.com");
         cy.get('[type="tel"]').type("68 992718050");
         cy.get(".alterar").click();
+        cy.wait(2000);
+        cy.screenshot("tela-alteracao-concluida", { overwrite: true });
     });
 
     it("Deve remover um contato", () => {
         cy.get(":nth-child(5) > .sc-gueYoa > .delete").click();
+        cy.wait(2000);
+        cy.screenshot("tela-remocao-concluida", { overwrite: true });
     });
 });
